a61b8bf855bb48fe51512e998653aa7e7e116a08
Ade Pangestu <adepanges@gmail.com>
2022-11-10 11:47:08 +0700

    fix ecosystem

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -3,7 +3,7 @@ module.exports = {
     {
       name: 'FE-Prerender-Service',
       script: './server.js',
-      instances: 2,
+      instances: 1,
       exec_mode: 'cluster',
       max_memory_restart: '500M',
       env: {
